c30044c4613d338b33800c667d70fac268a07e2e
taiyuezifeng <taiyuezifeng@163.com>
2016-04-15 22:22:05 +0800

    add HelloWorld.java
    2016-04-15
    Add the HelloWorld.java and commit the changes.

---
+++ src/org/git/demo/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.git.demo;
+
+/**
+ * Created by taiyuezifeng on 16-4-15.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+    }
+
+}
